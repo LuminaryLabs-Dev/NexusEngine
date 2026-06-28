c56d353b43752aca339801169e84d2ce27c0e99f
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-28 03:30:25 -0400

    refactor: add core graphics kit entrypoint

---
+++ src/core-kits/core-graphics-kit/index.js
@@ -0,0 +1,2 @@
+export { RenderDescriptorState, createRenderDescriptorKit, createRenderDescriptorSnapshot } from "../../render-descriptor-kit.js";
+export { createCoreGraphicsKit } from "../core-domains.js";
