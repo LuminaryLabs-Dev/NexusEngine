87c48bbaf0dc95d23474dc026e6b345befded8fc
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-06 07:12:51 -0400

    Add scene registry domain module

---
+++ src/core-kits/core-scene-kit/scene-registry.js
@@ -0,0 +1,8 @@
+export {
+  createSceneDescriptor,
+  createSceneExit,
+  createSceneRegistry,
+  createSceneRequirements,
+  hostBindingForSceneKind,
+  normalizeSceneExits
+} from "./descriptors.js";
