9d1ee1ecc2a2a5091ed5e38109ad2e0cb7f3be8b
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-08 10:02:11 -0400

    Export Nexus diffusion module

---
+++ src/modules/nexus-diffusion/index.js
@@ -0,0 +1,11 @@
+export * from "./nexus-diffusion-domain.js";
+
+export * from "./subdomains/dataset/index.js";
+export * from "./subdomains/tensor/index.js";
+export * from "./subdomains/backend/index.js";
+export * from "./subdomains/noise/index.js";
+export * from "./subdomains/model/index.js";
+export * from "./subdomains/training/index.js";
+export * from "./subdomains/sampling/index.js";
+export * from "./subdomains/checkpoint/index.js";
+export * from "./subdomains/preview/index.js";
