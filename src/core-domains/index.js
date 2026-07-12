55c8d152cbfb8be4cdebb6319ef17e78c58d9b27
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-12 11:19:26 -0400

    feat: export core motion and physics domains

--- src/core-domains/index.js
+++ src/core-domains/index.js
@@ -1,2 +1,4 @@
 export * from "./core-world-domain/index.js";
 export * from "./core-presentation-domain/index.js";
+export * from "./core-motion-domain/index.js";
+export * from "./core-physics-domain/index.js";
