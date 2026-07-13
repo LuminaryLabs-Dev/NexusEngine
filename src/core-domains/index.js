52c56af6bfc4f0fb5e04f86ec59e6db4db5af6b9
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-13 10:07:50 -0400

    feat: export Core Startup domain

--- src/core-domains/index.js
+++ src/core-domains/index.js
@@ -1,3 +1,4 @@
+export * from "./core-startup-domain/index.js";
 export * from "./core-world-domain/index.js";
 export * from "./core-presentation-domain/index.js";
 export * from "./core-motion-domain/index.js";
