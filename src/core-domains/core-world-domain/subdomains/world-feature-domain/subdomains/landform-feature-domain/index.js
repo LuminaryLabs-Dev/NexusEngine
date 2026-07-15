ee07f35a17695c203b3c0076a2eb8b467c3ce1b8
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-15 06:26:03 -0400

    feat(world): export landform feature catalog

--- src/core-domains/core-world-domain/subdomains/world-feature-domain/subdomains/landform-feature-domain/index.js
+++ src/core-domains/core-world-domain/subdomains/world-feature-domain/subdomains/landform-feature-domain/index.js
@@ -1,6 +1,3 @@
 export * from "./contracts.js";
 export * from "./landform-feature-domain.js";
-export * from "./kits/mountain-feature-kit/index.js";
-export * from "./kits/canyon-feature-kit/index.js";
-export * from "./kits/cliff-feature-kit/index.js";
-export * from "./kits/plateau-feature-kit/index.js";
+export * from "./kits/landform-feature-kits.js";
