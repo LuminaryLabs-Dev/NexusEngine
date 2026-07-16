19c2b0277b5ba56d5835591f38e97cf24327c09d
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-07-16 11:57:39 -0400

    feat(startup): export asset preparation bridge

--- src/core-domains/core-startup-domain/index.js
+++ src/core-domains/core-startup-domain/index.js
@@ -1,6 +1,7 @@
 import { createCoreStartupKit } from "../../core-kits/core-startup-kit/index.js";
 
 export * from "../../core-kits/core-startup-kit/index.js";
+export * from "./core-assets-startup-bridge.js";
 
 export function createCoreStartupDomain(config = {}) {
   return [createCoreStartupKit(config.root ?? config)];
